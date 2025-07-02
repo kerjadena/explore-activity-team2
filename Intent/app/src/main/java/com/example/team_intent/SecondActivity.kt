b7e56b4347def4ffe7b72af82b295ceb52fae43a
fifken <fifken69@gmail.com>
2025-07-02 17:02:18 +0700

    ✔️ upload intent challenge

---
+++ Intent/app/src/main/java/com/example/team_intent/SecondActivity.kt
@@ -0,0 +1,46 @@
+package com.example.team_intent
+
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class SecondActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        Log.d("SecondActivity", "onCreate")
+
+        val btnBack = findViewById<Button>(R.id.btnBack)
+        btnBack.setOnClickListener {
+            finish()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("SecondActivity", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("SecondActivity", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("SecondActivity", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("SecondActivity", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("SecondActivity", "onDestroy")
+    }
+}
